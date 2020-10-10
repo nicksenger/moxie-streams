8224eaf2be34b995267682a8647390aec985f856
Nick Senger <nicholas.senger@synthego.com>
2020-10-10 12:15:31 -0700

    chore: fix doctest formatting

--- src/lib.rs
+++ src/lib.rs
@@ -4,85 +4,83 @@ use std::rc::Rc;
 use futures::{channel::mpsc, future::ready, stream::Stream, StreamExt};
 use moxie::{load_once, once, state, Commit};
 
-/**
- * Root a state variable at the callsite, returning a `Rc<dispatch>` which may be used to send messages
- * to be processed by the provided `reducer` and `operator`.
- *
- * For each message dispatched, the provided `reducer` will be invoked with a reference to the current
- * state and the message. The state will then be updated with the return value from the `reducer`.
- *
- * The `operator` receives an `mpsc::UnboundedReceiver<Msg>` stream of messages which have _already_
- * been processed by the `reducer`, and returns a stream of messages which will begin the cycle over
- * again (piped to dispatch).
- *
- * Examples
- *
- * ```
- * use futures::{executor::LocalPool, StreamExt, future::ready};
- * use moxie::runtime::RunLoop;
- * use moxie_streams::mox_stream;
- *
- * #[derive(Clone)]
- * enum Msg {
- *     Ping,
- *     Pong,
- * }
- *
- * struct State {
- *     pings: usize,
- *     pongs: usize,
- * }
- *
- * let mut rt = RunLoop::new(|| {
- *     mox_stream(
- *         State { pings: 0, pongs: 0 },
- *         |state, msg| match msg {
- *             Msg::Ping => State {
- *                 pings: state.pings + 1,
- *                 pongs: state.pongs,
- *             },
- *             Msg::Pong => State {
- *                 pings: state.pings,
- *                 pongs: state.pongs + 1,
- *             },
- *         },
- *         |in_stream| {
- *             in_stream
- *                 .filter(|msg| {
- *                     ready(match msg {
- *                         Msg::Ping => true,
- *                         _ => false,
- *                     })
- *                 })
- *                 .map(|_| Msg::Pong)
- *         },
- *     )
- * });
- *
- * let mut exec = LocalPool::new();
- * rt.set_task_executor(exec.spawner());
- *
- * exec.run_until_stalled();
- *
- * let (first_commit, dispatch) = rt.run_once();
- * assert_eq!(first_commit.pings, 0);
- * assert_eq!(first_commit.pongs, 0);
- *
- * dispatch(Msg::Pong);
- * exec.run_until_stalled();
- *
- * let (second_commit, _) = rt.run_once();
- * assert_eq!(second_commit.pings, 0);
- * assert_eq!(second_commit.pongs, 1);
- *
- * dispatch(Msg::Ping);
- * exec.run_until_stalled();
- *
- * let (third_commit, _) = rt.run_once();
- * assert_eq!(third_commit.pings, 1);
- * assert_eq!(third_commit.pongs, 2);
- * ```
- */
+/// Root a state variable at the callsite, returning a `Rc<dispatch>` which may be used to send messages
+/// to be processed by the provided `reducer` and `operator`.
+///
+/// For each message dispatched, the provided `reducer` will be invoked with a reference to the current
+/// state and the message. The state will then be updated with the return value from the `reducer`.
+///
+/// The `operator` receives an `mpsc::UnboundedReceiver<Msg>` stream of messages which have _already_
+/// been processed by the `reducer`, and returns a stream of messages which will begin the cycle over
+/// again (piped to dispatch).
+///
+/// # Example
+///
+/// ```
+/// use futures::{executor::LocalPool, StreamExt, future::ready};
+/// use moxie::runtime::RunLoop;
+/// use moxie_streams::mox_stream;
+///
+/// #[derive(Clone)]
+/// enum Msg {
+///     Ping,
+///     Pong,
+/// }
+///
+/// struct State {
+///     pings: usize,
+///     pongs: usize,
+/// }
+///
+/// let mut rt = RunLoop::new(|| {
+///     mox_stream(
+///         State { pings: 0, pongs: 0 },
+///         |state, msg| match msg {
+///             Msg::Ping => State {
+///                 pings: state.pings + 1,
+///                 pongs: state.pongs,
+///             },
+///             Msg::Pong => State {
+///                 pings: state.pings,
+///                 pongs: state.pongs + 1,
+///             },
+///         },
+///         |in_stream| {
+///             in_stream
+///                 .filter(|msg| {
+///                     ready(match msg {
+///                         Msg::Ping => true,
+///                         _ => false,
+///                     })
+///                 })
+///                 .map(|_| Msg::Pong)
+///         },
+///     )
+/// });
+///
+/// let mut exec = LocalPool::new();
+/// rt.set_task_executor(exec.spawner());
+///
+/// exec.run_until_stalled();
+///
+/// let (first_commit, dispatch) = rt.run_once();
+/// assert_eq!(first_commit.pings, 0);
+/// assert_eq!(first_commit.pongs, 0);
+///
+/// dispatch(Msg::Pong);
+/// exec.run_until_stalled();
+///
+/// let (second_commit, _) = rt.run_once();
+/// assert_eq!(second_commit.pings, 0);
+/// assert_eq!(second_commit.pongs, 1);
+///
+/// dispatch(Msg::Ping);
+/// exec.run_until_stalled();
+///
+/// let (third_commit, _) = rt.run_once();
+/// assert_eq!(third_commit.pings, 1);
+/// assert_eq!(third_commit.pongs, 2);
+/// ```
 pub fn mox_stream<State: 'static, Msg: 'static + Clone, OutStream>(
     initial_state: State,
     reducer: impl Fn(&State, Msg) -> State + 'static,
